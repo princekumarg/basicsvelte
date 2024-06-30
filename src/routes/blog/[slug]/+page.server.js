0a65a771bedeefb2d9baff50e93debadffdf1f35
prince kumar <agarwal8789@gmail.com>
2024-06-30 20:12:05 +0530

    commit made on postgress

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,5 +1,24 @@
-export async function load({ params }) {
-    return {
-        content: `hello${params.slug}`
+import { sql } from "@vercel/postgres";
+import { createClient } from '@vercel/postgres';
+
+async function queryNames() {
+    const client = createClient();
+    await client.connect();
+    try {
+        const names = await client.sql`SELECT * FROM NAMES `;
+        return names;
+    } finally {
+        await client.end();
     }
 }
+export async function load({ locals }) {
+    return {
+        names: await queryNames()
+    }
+}
+
+// export async function load({ params }) {
+//     return {
+//         content: `hello${params.slug}`
+//     }
+// }
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,3 +3,9 @@
 </script>
 
 <p class="font-bold text-center text-green">{data.content}</p>
+
+<div>
+  {#each data.cart as { id, quantity } (id)}
+    <div>{id} - {quantity}</div>
+  {/each}
+</div>
