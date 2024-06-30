ce746d069e26026162754cb5428fd76156315b76
prince kumar <agarwal8789@gmail.com>
2024-06-30 19:32:32 +0530

    tailwind config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="hover:text-red-500 hover:underline pt-20 pl-20 font-bold">
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,4 +2,4 @@
   export let data;
 </script>
 
-<p>{data.content}</p>
+<p class="font-bold text-center text-green">{data.content}</p>
